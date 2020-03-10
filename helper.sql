73f42fb291910076b1ce0e862db63e360bdd0b3c
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-03-10 10:52:54 -0600

    working on pr_appts query

--- helper.sql
+++ helper.sql
@@ -2,3 +2,21 @@
 
 
 
+
+
+
+
+--pestroutes appts- 
+
+select pr_appt_id, customer_id, subscription_id, scheduled_date, pr_date_added, appt_status, status_text, is_initial, serviced_by, date_completed, office_name, service_desc from pestroutes_appts
+
+join office_joins on office_joins.proutes_id=pestroutes_appts.office_id 
+join pr_service_types on pr_service_types.pr_type_id=pestroutes_appts.service_type
+ where appt_status=1
+
+
+--pr_subs
+
+ select * from pr_subscriptions
+ join office_joins on office_joins.proutes_id=pr_subscriptions.office_id
+
